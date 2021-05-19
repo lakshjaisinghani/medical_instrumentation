c4999a9e84511dd8b3407527f5912508782e9ae1
Laksh Jaisinghani <jaisinghanilaksh@gmail.com>
2021-05-19 22:32:15 +1000

    feature: added filtering matlab code

---
+++ research_project/filtering.m
@@ -0,0 +1,23 @@
+fc    = 10; % Hz
+omega = 2*pi*fc;
+num   = omega;
+denum = [1 omega];
+
+
+H_LP          = tf(num, denum);
+H_LP_discrete = c2d(H_LP, 0.01);
+
+% Low pass -> 45 Hz
+% y(n+1) = 0.05916 * y(n) + 0.9408 * u(n)
+fc    = 500; % Hz
+omega = 2*pi*fc;
+num   = [1 0];
+denum = [1 omega];
+
+H_HP          = tf(num, denum);
+H_HP_discrete = c2d(H_HP, 0.01);
+
+H_LP_discrete.num
+H_LP_discrete.den
+H_HP_discrete.num
+H_HP_discrete.den
